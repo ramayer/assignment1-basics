2786835e6e54964d2c011ce6205ce005b6ea4517
Ron M <ramayer+git@gmail.com>
2025-08-26 21:24:03 -0700

    made test_silu_matches_pytorch pass

--- tests/adapters.py
+++ tests/adapters.py
@@ -471,6 +471,7 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
+    return ron_swiglu.SwiGLU(1,1).silu(in_features)
     raise NotImplementedError
 
 
